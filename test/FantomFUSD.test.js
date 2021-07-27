dfda8fd49e646560d9753d6ef36d164655bcf10e
prolingua <prolingua.geo@gmail.com>
2021-07-27 09:23:14 +0100

    test packages
    installed packages for testing (openzeppelin, chai)

---
+++ test/FantomFUSD.test.js
@@ -0,0 +1,29 @@
+const {
+    BN,
+    constants,
+    expectEvent,
+    expectRevert,
+  } = require('@openzeppelin/test-helpers')
+  const { ZERO_ADDRESS } = constants
+  
+  const { expect } = require('chai')
+  
+  const FantomFUSD = artifacts.require('FantomFUSD')
+
+  contract('Unit Test for FantomFUSD', function ([owner]){
+
+    const name = 'Fantom USD'
+    const symbol = 'FUSD'
+
+    beforeEach(async function () {
+        this.token = await FantomFUSD.new ({from:owner})
+        this.token.initialize(owner)
+    })
+
+    describe('metadata', function () {
+        it('has a name', async function () {
+
+        }
+    )
+    })
+  })
